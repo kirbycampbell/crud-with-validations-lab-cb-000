043995a2f2b10e77193b662dcff877ac68a18d72
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-18 17:09:21 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
 
 
   def create
-    @song = Song.new
+    @song = Song.new(song_params)
     if @song.save
       redirect_to song_path(@song)
     else
