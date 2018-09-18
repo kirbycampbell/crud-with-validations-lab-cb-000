8963dae0f484a7e3085e612590e9cc56c30e89a6
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-18 16:52:02 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,8 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
 
 
-  validates :release_year, presence: true, if: :released?
-  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year}
+  validates :release_year, presence: true, if: :released?, numericality: { less_than_or_equal_to: Date.today.year}
+
 
   def released?
     released
