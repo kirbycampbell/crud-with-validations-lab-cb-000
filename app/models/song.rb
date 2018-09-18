c5ab9f3bf05eb6868b73830fa9a9065c2622f659
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-18 16:45:56 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,13 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
+  validates :release_year, presence: true, unless: !released?
 
+  def released?
+    if self.released == true
+      true
+    else
+      false
+  end
+  
 
 end
