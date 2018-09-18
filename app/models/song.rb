bef9d39942e47a2d41fd66c83384ac2c404185af
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-18 16:57:00 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,10 +3,15 @@ class Song < ActiveRecord::Base
 
 
   validates :release_year, presence: true, if: :released?, numericality: { less_than_or_equal_to: Date.today.year}
-
+  validates :title, uniqueness: true, if: !artist_same?
 
   def released?
     released
+  end
+
+  def artist_same?
+    @song = Song.findby(:title)
+    @song.artist_name == artist_name
   end
 
 
